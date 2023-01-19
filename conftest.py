9597991aaeae7c529dde3db23dfeb8d8a94ca1f0
Zaur Bibilov <bgwrld77@gmail.com>
2023-01-19 21:28:21 +0300

    fixes

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
+from pages.base_page import BasePage
+from pages.login_page import LoginPage
 
 
-@pytest.fixture()
+@pytest.fixture
 def driver():
     print("\nstart browser for test")
     driver = webdriver.Chrome()
+
     yield driver
+
     print("\nquit browser")
     driver.quit()
--- test_main.py
+++ test_main.py
@@ -2,23 +2,24 @@ from selenium import webdriver
 from pages.base_page import BasePage
 from pages.login_page import LoginPage
 
-driver = webdriver.Chrome
 
-base_page = BasePage(driver)
-log_in_page = LoginPage(driver)
+def test_log_in_valid(driver):
+    base_page = BasePage(driver)
+    log_in_page = LoginPage(driver)
 
-
-def test_log_in_valid():
     base_page.open()
 
-    base_page.go_to(base_page.login_url)
+    base_page.go_to(log_in_page.login_url)
 
     log_in_page.log_in_valid()
 
 
-def test_log_in_invalid():
+def test_log_in_invalid(driver):
+    base_page = BasePage(driver)
+    log_in_page = LoginPage(driver)
+
     base_page.open()
 
-    base_page.go_to(base_page.login_url)
+    base_page.go_to(log_in_page.login_url)
 
     log_in_page.log_in_invalid()
