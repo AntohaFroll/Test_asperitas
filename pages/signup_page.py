7137c3a10b3210884f4089dc8065f7a461748f51
AntohaFroll <antohafroll@gmail.com>
2023-02-18 15:55:51 +0300

    signup tests complete

--- pages/signup_page.py
+++ pages/signup_page.py
@@ -16,47 +16,53 @@ class SignupPage(BasePage):
         assert self.wait_of_element_present(*BasePageLocators.CREATE_POST_LINK), \
             "Login failed (valid values)!"
 
-    def should_be_required_massage(self):
-        assert self.wait_of_element_present(*SignupPageLocators.REQUIRED_MESSAGE), \
-            "Login failed (no values)!"
-
     def empty_fields(self):
         self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
 
-    def entry_invalid_date(self, generate_username, generate_password, generate_text):
-        self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
+    def should_be_required_massage(self):
         assert self.wait_of_element_present(*SignupPageLocators.REQUIRED_MESSAGE), \
-            "Signup failed (no values)!"
+            "Login failed (no value/values)!"
 
+    def entry_invalid_username_and_empty_password_field(self):
         self.driver.find_element(*SignupPageLocators.USERNAME_FIELD).send_keys("$#%")
         self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
+
+    def entry_valid_username_and_empty_password_field(self, generate_username):
+        self.driver.find_element(*SignupPageLocators.USERNAME_FIELD).send_keys(generate_username)
+        self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
+
+    def should_be_contains_invalid_characters_message(self):
         assert self.wait_of_element_present(*SignupPageLocators.CONTAINS_INVALID_CHARACTERS_MESSAGE), \
             "Signup failed (invalid user name)!"
 
-        self.driver.refresh()
-        self.driver.find_element(*SignupPageLocators.USERNAME_FIELD).send_keys(generate_username)
-        self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
-        assert self.wait_of_element_present(*SignupPageLocators.REQUIRED_MESSAGE), \
-            "Signup failed (no password)!"
-
-        self.driver.refresh()
+    def entry_valid_username_and_short_password(self, generate_username, generate_password):
         self.driver.find_element(*SignupPageLocators.USERNAME_FIELD).send_keys(generate_username)
         self.driver.find_element(*SignupPageLocators.PASSWORD_FIELD).send_keys(generate_password[0])
         self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
+
+    def entry_valid_username_password_and_7_signs_confirm_password(self, generate_username,
+                                                                   generate_password):
+        self.driver.find_element(*SignupPageLocators.USERNAME_FIELD).send_keys(generate_username)
+        self.driver.find_element(*SignupPageLocators.PASSWORD_FIELD).send_keys(generate_password[0:7])
+        self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
+
+    def should_be_most_be_more_than_8_characters_message(self):
         assert self.wait_of_element_present(*SignupPageLocators.MUST_BE_MORE_THAN_8_CHARACTERS_MESSAGE), \
             "Signup failed (short password)!"
 
-        self.driver.refresh()
+    def entry_valid_username_password_and_empty_confirm_password_field(self, generate_username,
+                                                                       generate_password):
         self.driver.find_element(*SignupPageLocators.USERNAME_FIELD).send_keys(generate_username)
         self.driver.find_element(*SignupPageLocators.PASSWORD_FIELD).send_keys(generate_password)
         self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
-        assert self.wait_of_element_present(*SignupPageLocators.PASSWORD_MUST_MATCH_MESSAGE), \
-            "Signup failed (password not match)!"
 
-        self.driver.refresh()
+    def entry_valid_username_password_and_invalid_confirm_password_field(self, generate_username,
+                                                                         generate_password, generate_text):
         self.driver.find_element(*SignupPageLocators.USERNAME_FIELD).send_keys(generate_username)
         self.driver.find_element(*SignupPageLocators.PASSWORD_FIELD).send_keys(generate_password)
         self.driver.find_element(*SignupPageLocators.CONFIRM_PASSWORD_FIELD).send_keys(generate_text)
         self.driver.find_element(*SignupPageLocators.SIGNUP_BUTTON).click()
+
+    def should_be_password_must_match_message(self):
         assert self.wait_of_element_present(*SignupPageLocators.PASSWORD_MUST_MATCH_MESSAGE), \
             "Signup failed (password not match)!"
--- test_main.py
+++ test_main.py
@@ -16,22 +16,75 @@ def test_open_site(driver):
 class TestsSignup:
     def test_signup_valid(self, driver, generate_username, generate_password):
         base_page = BasePage(driver)
-        signup_page = SignupPage(driver)
         base_page.open()
         base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
         signup_page.entry_valid_date(generate_username, generate_password)
+        signup_page.should_be_authorized_user()
 
-    def test_signup_invalid(self, driver, generate_username, generate_password, generate_text):
+    def test_empty_fields(self, driver):
         base_page = BasePage(driver)
-        signup_page = SignupPage(driver)
         base_page.open()
         base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
         signup_page.empty_fields()
         signup_page.should_be_required_massage()
-        signup_page.entry_invalid_date(generate_username, generate_password, generate_text)
+
+    def test_invalid_username_and_empty_password_field(self, driver):
+        base_page = BasePage(driver)
+        base_page.open()
+        base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
+        signup_page.entry_invalid_username_and_empty_password_field()
+        signup_page.should_be_contains_invalid_characters_message()
+
+    def test_valid_username_and_empty_password_field(self, driver, generate_username):
+        base_page = BasePage(driver)
+        base_page.open()
+        base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
+        signup_page.entry_valid_username_and_empty_password_field(generate_username)
+        signup_page.should_be_required_massage()
+
+    def test_valid_username_and_short_password(self, driver, generate_username, generate_password):
+        base_page = BasePage(driver)
+        base_page.open()
+        base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
+        signup_page.entry_valid_username_and_short_password(generate_username, generate_password)
+        signup_page.should_be_most_be_more_than_8_characters_message()
+
+    def test_entry_valid_username_password_and_7_signs_password(self, driver, generate_username,
+                                                                generate_password):
+        base_page = BasePage(driver)
+        base_page.open()
+        base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
+        signup_page.entry_valid_username_password_and_7_signs_confirm_password(generate_username,
+                                                                               generate_password)
+        signup_page.should_be_most_be_more_than_8_characters_message()
+
+    def test_entry_valid_username_password_and_empty_confirm_password_field(self, driver, generate_username,
+                                                                            generate_password):
+        base_page = BasePage(driver)
+        base_page.open()
+        base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
+        signup_page.entry_valid_username_password_and_empty_confirm_password_field(generate_username,
+                                                                                   generate_password)
+        signup_page.should_be_password_must_match_message()
+
+    def test_entry_valid_username_password_and_invalid_confirm_password_field(self, driver, generate_username,
+                                                                              generate_password, generate_text):
+        base_page = BasePage(driver)
+        base_page.open()
+        base_page.go_to_signup_page()
+        signup_page = SignupPage(driver)
+        signup_page.entry_valid_username_password_and_invalid_confirm_password_field(generate_username,
+                                                                                     generate_password, generate_text)
+        signup_page.should_be_password_must_match_message()
 
 
-@pytest.mark.login
 class TestsLogin:
     def test_login_valid(self, driver, new_user):
         base_page = BasePage(driver)
@@ -79,7 +132,6 @@ class TestsPost:
 
 @pytest.mark.comment
 class TestsComment:
-    @pytest.mark.only
     def test_create_comment(self, driver, new_user, generate_text):
         base_page = BasePage(driver)
         login_page = LoginPage(driver)
